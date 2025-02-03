4e796bedc5facd88b3c7df99554886b5c93306e9
KIRAN AHMED <ahmedrazakiran1@gmail.com>
2025-02-03 20:29:33 +0500

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,18 @@
 const nextConfig = {
     images: {
         domains: ['cdn.sanity.io'],
-      },
+      }
 };
 
+// export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['cdn.sanity.io'],
+//   },
 export default nextConfig;
+//     runtime: 'nodejs', // Ensure it runs in Node.js instead of Edge
+//   },
+// };
+
+// export default nextConfig;
